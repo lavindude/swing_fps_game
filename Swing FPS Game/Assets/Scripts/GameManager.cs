e65d31e8d6df338233eb0905a57f68e952602885
Devadithya <DevaLav22@issaquah.wednet.edu>
2022-03-01 14:49:40 -0800

    started connecting to the graphql API

--- Swing FPS Game/Assets/Scripts/GameManager.cs
+++ Swing FPS Game/Assets/Scripts/GameManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Http;
+using System;
+//using UnityEngine.Networking;
+
+//use this: https://dev.to/mpetrinidev/call-github-graphql-api-using-c-50o5
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        //UnityWebRequest
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://swing-api.herokuapp.com/graphql")
+        };
     }
 
     // Update is called once per frame
