e7df483246ebc3e0df07a56c0e7a063ccaae4c02
Devadithya <DevaLav22@issaquah.wednet.edu>
2022-03-03 14:52:48 -0800

    wrote a script that connects to api, but there are some major bugs

--- Swing FPS Game/Assets/Scripts/GameManager.cs
+++ Swing FPS Game/Assets/Scripts/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Http;
 using System;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
 //using UnityEngine.Networking;
 
 //use this: https://dev.to/mpetrinidev/call-github-graphql-api-using-c-50o5
@@ -12,16 +15,48 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //UnityWebRequest
-        var httpClient = new HttpClient
-        {
-            BaseAddress = new Uri("https://swing-api.herokuapp.com/graphql")
-        };
+        Task t = WebConnection();
+        t.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    static async Task WebConnection()
+    {
+        //UnityWebRequest
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://swing-api.herokuapp.com/graphql")
+        };
+
+        var queryObject = new
+        {
+            query = @"query {
+                getConnectedPlayers {
+                    id
+                }
+            }"
+        };
+
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Post,
+            Content = new StringContent(JsonConvert.SerializeObject(
+                queryObject), Encoding.UTF8, "application/json")
+        };
+
+        dynamic responseObj;
+        using (var response = await httpClient.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseObj = JsonConvert.DeserializeObject<dynamic>(responseString);
+        }
+
+        Debug.Log(responseObj.data.getConnectedPlayers.id);
     }
 }
