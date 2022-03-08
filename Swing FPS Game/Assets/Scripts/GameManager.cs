fa44b0917505b307e789a4632439e4049ae84887
Devadithya <DevaLav22@issaquah.wednet.edu>
2022-03-08 14:42:21 -0800

    still many bugs

--- Swing FPS Game/Assets/Scripts/GameManager.cs
+++ Swing FPS Game/Assets/Scripts/GameManager.cs
@@ -3,20 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Http;
 using System;
-using Newtonsoft.Json;
+//using Newtonsoft.Json;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
 //using UnityEngine.Networking;
 
+
+//*** Check this: https://github.com/xoofx/UnityNuGet
 //use this: https://dev.to/mpetrinidev/call-github-graphql-api-using-c-50o5
 
 public class GameManager : MonoBehaviour
-{
+{/*
+    [DataContract]
+    public class JsonConverter
+    {
+        public JsonConverter() {}
+        public static string Serialize<T>(T item)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new DataContractJsonSerializer(typeof(T)).WriteObject(ms, item);
+                return Encoding.Default.GetString(ms.ToArray());
+            }
+        }
+
+        public static T Deserialize<T>(string body)
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write(body);
+                writer.Flush();
+                stream.Position = 0;
+                return (T)new DataContractJsonSerializer(typeof(T)).ReadObject(stream);
+            }
+        }
+    }*/
+
     // Start is called before the first frame update
     void Start()
     {
-        Task t = WebConnection();
-        t.Start();
+        WebConnection();
     }
 
     // Update is called once per frame
@@ -25,9 +55,8 @@ public class GameManager : MonoBehaviour
         
     }
 
-    static async Task WebConnection()
+    async void WebConnection()
     {
-        //UnityWebRequest
         var httpClient = new HttpClient
         {
             BaseAddress = new Uri("https://swing-api.herokuapp.com/graphql")
@@ -41,11 +70,11 @@ public class GameManager : MonoBehaviour
                 }
             }"
         };
-
+        
         var request = new HttpRequestMessage
         {
             Method = HttpMethod.Post,
-            Content = new StringContent(JsonConvert.SerializeObject(
+            Content = new StringContent(JsonConverter.Serialize(
                 queryObject), Encoding.UTF8, "application/json")
         };
 
@@ -54,9 +83,9 @@ public class GameManager : MonoBehaviour
         {
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
-            responseObj = JsonConvert.DeserializeObject<dynamic>(responseString);
+            responseObj = JsonConverter.Deserialize<dynamic>(responseString);
         }
 
-        Debug.Log(responseObj.data.getConnectedPlayers.id);
+        Debug.Log(responseObj.data.getConnectedPlayers.id); 
     }
 }
