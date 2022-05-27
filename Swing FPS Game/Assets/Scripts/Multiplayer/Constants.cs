c1e894ca91e48ddb68e963e68c039a66f21f23c9
Sahil Gupta <sahsahgupta@gmail.com>
2022-05-27 14:28:01 -0700

    adding pickup ui

--- Swing FPS Game/Assets/Scripts/Multiplayer/Constants.cs
+++ Swing FPS Game/Assets/Scripts/Multiplayer/Constants.cs
@@ -4,7 +4,7 @@ public static class Constants
 {
     /* We want these values to be dynamic in the future through lobbymaking.
        For now, they are hard coded. */
-    public static int playerId = 1;
+    public static int playerId = 4;
     public static int lobbyId = 1;
-    public static int[] otherPlayerIds = new int[] { 2, 3, 4 };
+    public static int[] otherPlayerIds = new int[] { 1, 2, 3 };
 }
